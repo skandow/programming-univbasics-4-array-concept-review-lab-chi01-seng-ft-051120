46b98946ab0b55617a40a2d35743e32b0a502533
Steven Kandow <kando1sr@gmail.com>
2020-02-21 19:42:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,8 @@ def find_element_index(array, value_to_find)
   while counter < array.length do 
     if array[counter] == value_to_find
       final_counter = counter
-      counter += 1 
     end
+    counter += 1
   end
   final_counter ? return final_counter : return nil 
 end
